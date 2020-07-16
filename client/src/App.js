4b8f429c1b7ae8d438da589ac512699b00810c5d
kujebe <kujebeseun@gmail.com>
2020-07-17 00:41:04 +0100

    Cleaned up root and public directories, imported favicons, added the dir and inserted global styles to index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import useRequest from "utils/swr-hook";
-import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
   const { data, error } = useRequest("/teachers");
@@ -11,24 +9,7 @@ function App() {
   if (error) {
     console.log(error);
   }
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div> Welcome</div>;
 }
 
 export default App;
