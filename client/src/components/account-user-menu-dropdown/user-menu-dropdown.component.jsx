058bbf7a615a46b0d95946d3eb531ace317c73d2
kujebe <kujebeseun@gmail.com>
2020-08-19 12:20:27 +0100

    small fixes

--- client/src/components/account-user-menu-dropdown/user-menu-dropdown.component.jsx
+++ client/src/components/account-user-menu-dropdown/user-menu-dropdown.component.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
 import { signOutStart } from "store/user/user-actions";
 
 import styles from "./user-menu-dropdown.module.scss";
@@ -9,9 +8,7 @@ const UserMenuDropdown = () => {
   const dispatch = useDispatch();
   return (
     <div className={styles.menu_dropdown}>
-      <Link as="div" onClick={() => dispatch(signOutStart())}>
-        Sign Out
-      </Link>
+      <div onClick={() => dispatch(signOutStart())}>Sign Out</div>
     </div>
   );
 };
