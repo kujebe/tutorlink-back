6033e29353fd49c9a5d2abbb3862f7ac0e88b056
kujebe <kujebeseun@gmail.com>
2020-08-19 12:32:56 +0100

    Redirect to home on logout

--- client/src/components/account-user-menu-dropdown/user-menu-dropdown.component.jsx
+++ client/src/components/account-user-menu-dropdown/user-menu-dropdown.component.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { signOutStart } from "store/user/user-actions";
 
 import styles from "./user-menu-dropdown.module.scss";
 
 const UserMenuDropdown = () => {
+  let history = useHistory();
   const dispatch = useDispatch();
+
+  const logout = () => {
+    dispatch(signOutStart());
+    history.push("/");
+  };
+
   return (
     <div className={styles.menu_dropdown}>
-      <div onClick={() => dispatch(signOutStart())}>Sign Out</div>
+      <div onClick={logout}>Sign Out</div>
     </div>
   );
 };
