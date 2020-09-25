5f38bd2ebbfba16ba1e27abed20f7f4db450aca3
kujebe <kujebeseun@gmail.com>
2020-09-25 23:36:43 +0100

    Styles children list in right section

--- client/src/components/customer-dashboard/right-section-children-list.component.jsx
+++ client/src/components/customer-dashboard/right-section-children-list.component.jsx
@@ -8,6 +8,8 @@ const CustomerChildrenList = () => {
     (state) => state.customer.customerData.customerChildren
   );
 
+  console.log(children);
+
   return (
     <div className={styles.root}>
       <div className={styles.head}>
@@ -15,8 +17,51 @@ const CustomerChildrenList = () => {
         <span className={styles.count}>{`(${children.length})`}</span>
       </div>
       <div className={styles.content}>
+        {children.length > 0 && (
+          <div className={`${styles.child_row} ${styles.head}`}>
+            <div className={`${styles.child_cell} ${styles.small_cell}`}>
+              S/N
+            </div>
+            <div className={`${styles.child_cell} ${styles.big_cell}`}>
+              Name
+            </div>
+            <div className={`${styles.child_cell} ${styles.big_cell}`}>
+              School
+            </div>
+            <div className={`${styles.child_cell} ${styles.small_cell}`}>
+              Age
+            </div>
+            <div className={`${styles.child_cell} ${styles.small_cell}`}>
+              Class
+            </div>
+            <div className={`${styles.child_cell} ${styles.small_cell}`}>
+              Gender
+            </div>
+          </div>
+        )}
         {children.length > 0
-          ? children.map((child) => child.fullname)
+          ? children.map((child, index) => (
+              <div key={child._id} className={styles.child_row}>
+                <div className={`${styles.child_cell} ${styles.small_cell}`}>
+                  {index + 1}
+                </div>
+                <div className={`${styles.child_cell} ${styles.big_cell}`}>
+                  {child.fullname}
+                </div>
+                <div className={`${styles.child_cell} ${styles.big_cell}`}>
+                  {child.school}
+                </div>
+                <div className={`${styles.child_cell} ${styles.small_cell}`}>
+                  {child.age}
+                </div>
+                <div className={`${styles.child_cell} ${styles.small_cell}`}>
+                  {child.class}
+                </div>
+                <div className={`${styles.child_cell} ${styles.small_cell}`}>
+                  {child.gender}
+                </div>
+              </div>
+            ))
           : "No registerd  child"}
       </div>
     </div>
