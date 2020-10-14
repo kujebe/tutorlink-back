54756502f3e7a369452194b7870ccd9cf4958543
kujebe <kujebeseun@gmail.com>
2020-10-14 14:40:34 +0100

    Added react-select and created time checkout time select field

--- client/src/components/checkout-steps/step-one.component.jsx
+++ client/src/components/checkout-steps/step-one.component.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
+import Select from 'react-select'
 import { format, addDays, differenceInCalendarMonths } from "date-fns";
 
 import FormInput from "components/form-input/form-input.component";
 
 import { ReactComponent as CalendarIcon } from "assets/images/calendar-icon.svg";
+import { ReactComponent as TimeIcon } from "assets/images/time-icon.svg";
 
 import styles from "./checkout-steps.module.scss";
 import "react-datepicker/dist/react-datepicker.css";
@@ -13,32 +15,47 @@ const StepOne = (props) => {
     // console.log(props)
     const [state, setState] = useState({
         startDate: new Date(),
-        enDate: addDays(new Date(), 1),
-        fullname: "",
+        enDate: "",
+        startTime: "",
     });
 
     const ref = React.createRef();
+    const selectOptions = [
+        { value: '1', label: '1 Hour' },
+        { value: '2', label: '2 Hour' },
+        { value: '3', label: '3 Hour' },
+        { value: '4', label: '4 Hour' },
+        { value: '5', label: '5 Hour' },
+        { value: '6', label: '6 Hour' },
+        { value: '7', label: '7 Hour' },
+        { value: '8', label: '8 Hour' },
+        { value: '8', label: '8 Hour' },
+        { value: '10', label: '9 Hour' },
+    ];
 
-    const handleChange = (e) => {
-        // const { name, value } = e.target;
-        // setState({ ...state, [name]: value });
-    };
-
-    const onDateChange = (dates) => {
-        const [start, end] = dates;
-        setState({
-            ...state,
-            startDate: start,
-            endDate: end,
-        });
-    };
-
-    const DateInput = React.forwardRef((props, ref) => {
-        console.log(props)
+    const StartDateInput = React.forwardRef((props, ref) => {
         return (
             <div className={styles.date_input} onClick={props.onClick} ref={ref}>
-                <FormInput type="text" label="Select Date" value={props.value} onChange={() => null} />
-                <CalendarIcon className={styles.calendar_icon} />
+                <FormInput type="text" label="Start Date" value={props.value} onChange={() => null} />
+                <CalendarIcon className={styles.input_icon} />
+            </div>
+        )
+    });
+
+    const EndDateInput = React.forwardRef((props, ref) => {
+        return (
+            <div className={styles.date_input} onClick={props.onClick} ref={ref}>
+                <FormInput type="text" label="End Date" value={props.value} onChange={() => null} />
+                <CalendarIcon className={styles.input_icon} />
+            </div>
+        )
+    });
+
+    const TimeInput = React.forwardRef((props, ref) => {
+        return (
+            <div className={styles.date_input} onClick={props.onClick} ref={ref}>
+                <FormInput type="text" label="Start Time" value={props.value} onChange={() => null} />
+                <TimeIcon className={styles.input_icon} />
             </div>
         )
     });
@@ -54,7 +71,7 @@ const StepOne = (props) => {
                         endDate={state.endDate}
                         selectsStart
                         minDate={new Date()}
-                        customInput={<DateInput ref={ref} />}
+                        customInput={<StartDateInput ref={ref} />}
                     />
                 </div>
                 <div className={styles.col_two}>
@@ -65,20 +82,32 @@ const StepOne = (props) => {
                         endDate={state.endDate}
                         selectsEnd
                         minDate={addDays(state.startDate, 1)}
-                        customInput={<DateInput ref={ref} />}
+                        customInput={<EndDateInput ref={ref} />}
                     />
                 </div>
 
             </div>
             <div className={styles.two_cols}>
                 <div className={styles.col_one}>
-                    <FormInput
-                        type="text"
-                        name="fullname"
-                        value={state.fullname}
-                        onChange={handleChange}
-                        label="Full name"
-                        required
+                    <DatePicker
+                        selected={state.startTime}
+                        onChange={date => setState({ ...state, startTime: date })}
+                        showTimeSelect
+                        showTimeSelectOnly
+                        timeIntervals={60}
+                        // timeCaption="Time"
+                        dateFormat="h:mm aa"
+                        customInput={<TimeInput ref={ref} />}
+                    />
+                </div>
+
+                <div className={styles.col_two}>
+                    <Select
+                        options={selectOptions}
+                        isMulti
+                        styles={{ control: (styles) => ({ ...styles, margin: "20px 0", borderTop: "none", borderRight: "none", borderLeft: "none", bordeRadius: "none" }) }}
+                        isClearable={true}
+                        isSearchable={true}
                     />
                 </div>
             </div>
--- client/src/components/form-wizard-steps/form-wizard-steps.component.jsx
+++ client/src/components/form-wizard-steps/form-wizard-steps.component.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
+import styles from "./form-wizard-steps.module.scss";
+
 const Steps = ({ SW: { previousStep, nextStep, state: { activeStep } } }) => {
 
     return (
-        <div>
-            <h4>Control from outside component</h4>
+        <div className={styles.steps}>
+            {/*<h4>Control from outside component</h4>*/}
             <button onClick={previousStep}>Previous</button>
             <button onClick={nextStep}>Next</button>
         </div>
