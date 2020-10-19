fc238d910eb3e5f4fa4b8f08aa407f385d8a1504
kujebe <kujebeseun@gmail.com>
2020-10-19 19:59:30 +0100

    Implemented add child modal and select existing children on check out

--- client/src/components/checkout-steps/child-data-step.component.jsx
+++ client/src/components/checkout-steps/child-data-step.component.jsx
@@ -1,20 +1,59 @@
-import React from "react";
+import React, { useState, Fragment } from "react";
 import { useSelector } from "react-redux";
+import Select from 'react-select';
 
-import AddChildForm from "components/customer-dashboard/modal-add-child.component";
+import PlusIcon from "components/plus-icon/plus-icon.component";
+import Modal from "components/modal/modal.component";
+import AddChildModal from "components/customer-dashboard/modal-add-child.component";
+
+const selectStyles = {
+    control: (styles) => (
+        {
+            ...styles,
+            margin: "20px 0",
+            borderTop: "none",
+            borderRight: "none",
+            borderBottom: "1px solid #aaaaaa",
+            borderLeft: "none",
+            borderRadius: "none",
+            boxShadow: "none"
+        }
+    )
+}
 
 const ChildDataStep = () => {
+    const [state, setState] = useState({
+        children: []
+    })
+    const [openAddChildModal, setOpenAddChildModal] = useState(false);
     const children = useSelector((state) => state.customer.customerData.customerChildren);
-    if (children.length > 0) {
-        return (
-            // children.map(child => (
-            //     <div>{child.fullname}</div>
-            // ))
-            <AddChildForm />
-        )
-    } else {
-        return <AddChildForm />
-    }
+
+    const childrenOptions =
+        children.map(child => (
+            { value: child._id, label: child.fullname }
+        ));
+
+    return (
+        <Fragment>
+            <Select
+                options={childrenOptions}
+                isMulti
+                styles={selectStyles}
+                isClearable={true}
+                isSearchable={true}
+                placeholder="Select Children"
+                onChange={(selectedOptions) => setState({ ...state, children: selectedOptions })}
+            />
+            <PlusIcon action={setOpenAddChildModal} status={true} />
+
+            {openAddChildModal && (
+                <Modal>
+                    <AddChildModal closeModal={setOpenAddChildModal} />
+                </Modal>
+            )}
+        </Fragment>
+    )
+
 }
 
 export default ChildDataStep;
